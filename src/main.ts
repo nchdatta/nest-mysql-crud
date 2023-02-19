38a5ff37c1cdbd8eb4cc56096cbbd0d2cf6e28f3
Nayan Chandra Datta <nayanchdatta02@gmail.com>
2023-02-19 10:34:06 +0600

    updated

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  console.log(5000)
   await app.listen(process.env.PORT);
 }
 bootstrap();
